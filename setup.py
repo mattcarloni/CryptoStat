306146647e7971c838e6751d0020f9aee55ad490
mattcarloni <57003459+mattcarloni@users.noreply.github.com>
2023-02-10 22:29:59 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CryptoStat',
+    version='1.0',
+    packages=['app'],
+    url='',
+    license='',
+    author='matteocarloni',
+    author_email='',
+    description=''
+)
